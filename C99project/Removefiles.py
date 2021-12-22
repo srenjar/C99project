b30c6561d01558beb9c532d0465a98300a2bfc80
srenjar <75938323+srenjar@users.noreply.github.com>
2021-12-22 19:37:07 +0530

    Add files via upload

---
+++ C99project/Removefiles.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+import time
+
+def main():
+
+    deleted_folders_count = 0
+    deleted_files_count = 0
+
+    path = 'PATH_TO_DELETE'
+
+    days = 30
+
+    seconds = time.time() - (days * 24 * 60 * 60)
+
+    if os.path.exists(path) :
+
+     for root_folder , folders , files in os.walk(path) :
+          
+          if seconds >= get_file_or_folder_age(root_folder):
+              remove_folder(root_folder)
+              deleted_folders_count +=1
+
+              break
+            
+            else :
+                for folders in folders :
+                    folder_path = os.path.join(root_folder,folder)
+
+                    if seconds>= get_file_or_folder_age(folder_path):
+                        remove_folder (folder_path)
+                        deleted_folders_count +=1
+        
+            for files in files :
+                file+path = os.path.join(root_folder,file)
+
+                if seconds >= get_file_or_folder_age(file_path):
+                    remove_file(file_path)
+                    deleted_files_count +=1
+
+        else :
+             if seconds >= get_file_or_folder_age(path):
+                 remove_file(path)
+                 deleted_files_count +=1
+            
+        else : 
+            print( f'"{path}" is not found')
+            deleted_files_count +=1
+
+        print(f'total folders deleted :{deleted_folders_count}')
+        print(f'total files deleted :{deleted_files_count}')
+
+
+def remove_folder (path):
+    if not shutil.rmtree(path):
+        print(f"{path} is removed successfully")
+
+    else :
+        print('Unable to delete the file')
+
+def remove_file(path) :
+    if not os.remove(path) :
+        print(f'{path} is removed succesfully')
+
+    else :
+        print('Unable to delte the file')
+
+def get_file_or_folder_age(path):
+    ctime = os.start(path).st_ctime
+    return ctime
+
+
+if __ name __ == '__main__' :
+    main()
